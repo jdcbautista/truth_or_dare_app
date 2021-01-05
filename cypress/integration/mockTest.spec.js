bbee5f41bb3c6abb1d82c400d179764024c272aa
manthony <41161951+manthonyg@users.noreply.github.com>
2021-01-05 11:34:18 -0600

    deleted defdult tests and created a test sample

---
+++ cypress/integration/mockTest.spec.js
@@ -0,0 +1,21 @@
+describe("Sample integration test for Truth or Dare application that proves it is the best project", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
+  it("Truth or Dare is the best app", () => {
+    const truthOrDare = 9999;
+    const otherProjects = 0;
+    cy.expect(truthOrDare).to.be.greaterThan(otherProjects);
+  });
+});
+
+/**
+ * @description SOME HELPFUL DOCS TO WRITE TESTS!
+ *
+ * https://www.cypress.io/
+ * https://docs.cypress.io/guides/getting-started/writing-your-first-test.html
+ *
+ * Cheatsheet:
+ * https://github.com/janmanfai/cypress-cheat-sheet
+ */
