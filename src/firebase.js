bf9d601048cbc6574e9c0e37a93043bc1fe24736
manthony <41161951+manthonyg@users.noreply.github.com>
2021-01-10 22:01:26 -0600

    firebase stuff

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   projectId: "firestore-project-a4b07",
   storageBucket: "firestore-project-a4b07.appspot.com",
   messagingSenderId: "423972607278",
-  appId: "1:423972607278:web:7457d57f2227c1b5e016bf"
+  appId: "1:423972607278:web:7457d57f2227c1b5e016bf",
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -19,88 +19,80 @@ export const authenticateAnonymously = () => {
   return firebase.auth().signInAnonymously();
 };
 
-
 /**
  * @description getGame
  * Returns a single game object based on gameId
  * @params gameId - {string} - the id of the targeted game
  */
 export const getGame = async (gameId) => {
-  const snapshot = await db
-  .collection("game")
-  .doc(gameId)
-  .get();
-  return snapshot
+  const snapshot = await db.collection("game").doc(gameId).get();
+  return snapshot;
 };
-
 
 /**
  * @description getGames
  * Queries all rooms and returns an array of all game documents
  */
-export const getlGames = async () => {
-  const snapshot = await db
-  .collection("rooms")
-  .get()
-  return snapshot.docs.map(doc => doc.data())
-}
-
+export const getAllGames = async () => {
+  const snapshot = await db.collection("rooms").get();
+  return snapshot.docs.map((doc) => doc.data());
+};
 
 /**
- * @description addPlayer 
- * Adds a  new player to the targeted game 
- * @params userName - {string} - the id of the targeted user 
+ * @description addPlayer
+ * Adds a  new player to the targeted game
+ * @params userName - {string} - the id of the targeted user
  * @params userId - {string} - the id of the targeted user
  */
 export const addPlayer = async (newPlayer, gameId) => {
-  const {userId, username} = newPlayer
+  const { userId, username } = newPlayer;
   const snapshot = await db
-  .collection("rooms")
-  .doc(gameId)
-  .collection("players")
-  .doc(userId)
-  .set({
-    id: userId,
-    username: username,
-    ready: false,
-    score: 0,
-  });
-  return snapshot
+    .collection("rooms")
+    .doc(gameId)
+    .collection("players")
+    .doc(userId)
+    .set({
+      id: userId,
+      username: username,
+      ready: false,
+      score: 0,
+    });
+  return snapshot;
 };
 
 /**
  * @description getPlayers
- * Queries all players from a targeted game and returns an array of all player documents 
- * @params gameId - {string} - the id of the targeted game 
+ * Queries all players from a targeted game and returns an array of all player documents
+ * @params gameId - {string} - the id of the targeted game
  */
 export const getPlayers = async (gameId) => {
   const snapshot = await db
-  .collection("rooms")
-  .doc(gameId)
-  .collection("players")
+    .collection("rooms")
+    .doc(gameId)
+    .collection("players");
 
-  return snapshot
+  return snapshot;
   // return snapshot.docs.map(doc => doc.data())
-}
+};
 
 /**
- * @description readyPlayer 
- * Adds a  new player to the targeted game 
- * @params updatedPlayer - {string} - the updated player object 
- * @params gameId - {string} - the id of the targeted game 
+ * @description readyPlayer
+ * Adds a  new player to the targeted game
+ * @params updatedPlayer - {string} - the updated player object
+ * @params gameId - {string} - the id of the targeted game
  */
 export const readyPlayer = async (userId, gameId) => {
-  console.log(userId)
+  console.log(userId);
   // const {userId} = updatedPlayer
   const snapshot = await db
-  .collection("rooms")
-  .doc(gameId)
-  .collection('players')
-  .doc(userId)
-  .update({
-    ready: true
-  });
-  return snapshot
+    .collection("rooms")
+    .doc(gameId)
+    .collection("players")
+    .doc(userId)
+    .update({
+      ready: true,
+    });
+  return snapshot;
 };
 
 // export const editPlayer = (updatedPlayer) => {
