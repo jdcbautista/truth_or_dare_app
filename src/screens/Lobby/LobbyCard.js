afa1e2f6288c19dd1fba8da34ece8582b7e0995e
manthony <41161951+manthonyg@users.noreply.github.com>
2021-01-10 22:02:21 -0600

    lobby components and styles

--- src/screens/Lobby/LobbyCard.js
+++ src/screens/Lobby/LobbyCard.js
@@ -3,75 +3,89 @@ import Participant from "../Participant";
 import { getRandomInt } from "../../helpers";
 import {
   StyledCard,
-  StyledFlex,
-  StyledAvatar,
   StyledHeading,
   StyledBox,
-  StyledButtonContainer,
-  StyledButton,
+  StyledReadyButton,
   StyledBadge,
+  StyledAvatar,
   StyledCheckIcon,
   StyledCloseIcon,
 } from "./LobbyStyles";
 import { Box } from "rebass";
 
-function LobbyCard({ playerInfo, twilioUserInfo, userId }) {
-  console.log({ twilioUserInfo });
+function LobbyCard({
+  playerInfo,
+  twilioUserInfo,
+  userId,
+  handleReadyClick,
+  defaultCard,
+}) {
   return (
-    <Box p={3} width={1 / 4} color="white" bg="primary">
-      <StyledCard
-        width={256}
-        sx={{
-          borderRadius: 8,
-          boxShadow: "0 0 16px rgba(0, 0, 0, .25)",
-        }}
-      >
-        {/* <StyledAvatar
-          src={`https://robohash.org/${
-            playerInfo.username
-          }/size=100x100?set=set${getRandomInt(4)}`}
-          sx={{
-            width: 100,
-            height: 100,
-            borderRadius: 9999,
-          }}
-        /> */}
-        {twilioUserInfo && (
-          <Participant
-            key={twilioUserInfo?.sid}
-            participant={twilioUserInfo}
-            userId={userId}
-          />
-        )}
-        <StyledHeading>{playerInfo?.username}</StyledHeading>
-        {playerInfo?.ready ? (
-          <StyledBox
+    <>
+      {defaultCard ? (
+        <Box p={3} width={1 / 4} color="white" bg="primary">
+          <StyledCard
+            width={256}
             sx={{
-              maxWidth: 512,
-              mx: "auto",
-              px: 3,
+              borderRadius: 8,
+              boxShadow: "0 0 16px rgba(0, 0, 0, .25)",
             }}
           >
-            <StyledCheckIcon />
-          </StyledBox>
-        ) : (
-          <StyledBox
+            <StyledAvatar
+              displayoff={false}
+              src={`https://robohash.org/${Math.random() * 2000}/size=200x300`}
+              sx={{
+                height: 310,
+              }}
+            />
+
+            <StyledBadge sx={{ mx: "auto" }}>Waiting for player...</StyledBadge>
+          </StyledCard>
+        </Box>
+      ) : (
+        <Box p={3} width={1 / 4} color="white" bg="primary">
+          <StyledCard
+            width={256}
             sx={{
-              maxWidth: 512,
-              mx: "auto",
-              px: 3,
+              borderRadius: 8,
+              boxShadow: "0 0 16px rgba(0, 0, 0, .25)",
             }}
           >
-            <StyledCloseIcon />
-          </StyledBox>
-        )}
-        <StyledBadge sx={{ mx: "auto" }}>
-          {playerInfo?.ready
-            ? `${playerInfo?.username} ready!`
-            : `${playerInfo?.username} not ready!`}
-        </StyledBadge>
-      </StyledCard>
-    </Box>
+            {twilioUserInfo && userId && (
+              <Participant
+                key={twilioUserInfo?.sid}
+                participant={twilioUserInfo}
+                userId={userId}
+              />
+            )}
+
+            <StyledHeading>{playerInfo?.username}</StyledHeading>
+
+            {playerInfo?.ready ? (
+              <StyledBox>
+                <StyledCheckIcon />
+              </StyledBox>
+            ) : (
+              <StyledBox>
+                <StyledCloseIcon />
+              </StyledBox>
+            )}
+
+            {twilioUserInfo && !playerInfo?.ready && (
+              <StyledReadyButton onClick={handleReadyClick}>
+                I'm Ready!
+              </StyledReadyButton>
+            )}
+
+            <StyledBadge sx={{ mx: "auto" }}>
+              {playerInfo?.ready
+                ? `${playerInfo?.username} ready!`
+                : `${playerInfo?.username} not ready!`}
+            </StyledBadge>
+          </StyledCard>
+        </Box>
+      )}
+    </>
   );
 }
 
---
+++ src/screens/Lobby/LobbyInput.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "react-dom";
+import { FormGroup, StyledLabel, StyledInput, Message } from "./LobbyStyles";
+
+const message = "this is the validation message";
+function LobbyInput({ handleChange, handleSubmit }) {
+  return (
+    <div>
+      <FormGroup>
+        <form onSubmit={handleSubmit}>
+          <StyledLabel htmlFor="label">Label</StyledLabel>
+          <StyledInput onChange={handleChange} id="label" />
+          <Message>This is the validation message</Message>
+        </form>
+      </FormGroup>
+    </div>
+  );
+}
+
+export default LobbyInput;
