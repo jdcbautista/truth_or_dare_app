6147213936d8bc7fb55f05cfa376c4465a86f741
manthony <41161951+manthonyg@users.noreply.github.com>
2021-01-18 15:06:47 -0600

    added animation to modal

--- src/screens/Lobby/components/Navbar.js
+++ src/screens/Lobby/components/Navbar.js
@@ -52,7 +52,33 @@ function Navbar({
         <StyledModal isOpen={modalOpen} onBackgroundClick={toggleModal}>
           <Flex>
             <Box>
-              <StyledModalText size="lg">Game Rules</StyledModalText>
+              <StyledModalText size="md">Game Rules:</StyledModalText>
+              <StyledModalText size="sm">
+                The hotseat player who will select a truth or dare card will be
+                outlined in gold.
+              </StyledModalText>
+              <StyledModalText size="sm">
+                Game will end when a player reaches X points.
+              </StyledModalText>
+              <StyledModalText size="md">Instructions:</StyledModalText>
+              <StyledModalText size="sm">
+                1. Non-Hotseat players chooses a truth or dare card to play from
+                their hand.
+              </StyledModalText>
+              <StyledModalText size="sm">
+                2. Hotseat Player will choose one card to execute.
+              </StyledModalText>
+              <StyledModalText size="sm">
+                3. Non-hotseat players will vote on whether or not task was
+                completed successfully.
+              </StyledModalText>
+              <StyledModalText size="sm">
+                4. Points will be added to hotseat player score if approved by
+                non-hotseat players.
+              </StyledModalText>
+              <StyledModalText size="sm">
+                5. Turn will end and next hotseat player will be selected.
+              </StyledModalText>
             </Box>
           </Flex>
         </StyledModal>
